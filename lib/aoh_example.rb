8fc0dd49a1a49cc669204f7fb60c6029066f5e8b
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-05 17:16:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,6 +8,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+end
 
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
